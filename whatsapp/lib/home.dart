43dfd202da112e4dbeab3f49ae6e1509a2fd51ca
Ananthu Krishna P K <developer.02@ictkerala.org>
2024-06-23 13:25:08 +0530

    add changes

--- whatsapp/lib/home.dart
+++ whatsapp/lib/home.dart
@@ -26,17 +26,30 @@ class Home extends StatelessWidget {
           )
         ],
       ),
-      body:const Center(
-        child: Text(
-          "hello",
-          style: TextStyle(
-            color: Colors.red,
-            fontSize: 30,
-            fontWeight: FontWeight.bold,
-            fontStyle: FontStyle.normal
-          ),
-        ),
-      ),
+      // body:const Column(
+      //   mainAxisAlignment: MainAxisAlignment.start,//top to bottom
+      //  crossAxisAlignment: CrossAxisAlignment.center ,//left to right
+      //   children:[
+      //     SizedBox(
+      //       // height: 100,
+      //       // width: 200,
+      //     ),
+      //     Text(
+      //     "hello",
+      //     style: TextStyle(
+      //       color: Colors.red,
+      //       fontSize: 30,
+      //       fontWeight: FontWeight.bold,
+      //       fontStyle: FontStyle.normal
+      //     ),
+      //   ),
+
+      //   Icon(Icons.search)
+      //   ],
+      // ),
+
+
+      body: Row(),
     );
   }
 }
